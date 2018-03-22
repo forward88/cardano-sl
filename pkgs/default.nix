173549ea0836ce959c73190380803c3e44f1dc72
Marcin Szamotulski <marcin.szamotulski@iohk.io>
2018-03-22 18:26:47 +0100

    [CSL-2346] Updated pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7423,9 +7423,9 @@ inherit (pkgs) mesa;};
            license = stdenv.lib.licenses.mit;
          }) {};
       "cardano-sl-node" = callPackage
-        ({ mkDerivation, base, cardano-sl, cardano-sl-networking
-         , cardano-sl-ssc, cardano-sl-update, cardano-sl-util, cpphs
-         , log-warper, stdenv, universum
+         ({ mkDerivation, base, cardano-sl, cardano-sl-infra
+         , cardano-sl-networking, cardano-sl-ssc, cardano-sl-update
+         , cardano-sl-util, cpphs, log-warper, stdenv, universum
          }:
          mkDerivation {
            pname = "cardano-sl-node";
@@ -7434,8 +7434,9 @@ inherit (pkgs) mesa;};
            isLibrary = false;
            isExecutable = true;
            executableHaskellDepends = [
-             base cardano-sl cardano-sl-networking cardano-sl-ssc
-             cardano-sl-update cardano-sl-util log-warper universum
+             base cardano-sl cardano-sl-infra cardano-sl-networking
+             cardano-sl-ssc cardano-sl-update cardano-sl-util log-warper
+             universum
            ];
            executableToolDepends = [ cpphs ];
            doHaddock = false;
